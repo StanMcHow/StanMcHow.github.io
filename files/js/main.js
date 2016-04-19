c7cc9602ec233c7714645968e090a80184d8c94d
StanMcHow <makhov.stanislav@gmail.com>
2016-04-19 18:02:29 +0300

    Update main.js

--- files/js/main.js
+++ files/js/main.js
@@ -1,8 +1,10 @@
 $(".switch").click(function(e){e.preventDefault();$("#change").toggleClass("toggled");});
-$(function() {
+
   $("#search-button, #search-icon").click(function(e) {
     e.preventDefault();
-
     $("#search-button, #search-icon, #search-form").toggle();
   });
-});
+$("#add-medication, .invis").click(function(e) {
+    e.preventDefault();
+    $("#add-medication, .add-medication").toggle();
+  });
