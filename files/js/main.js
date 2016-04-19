06caa17c64a43fb1505f75053b5c5d4505443abe
StanMcHow <makhov.stanislav@gmail.com>
2016-04-19 14:44:05 +0300

    Added files via upload

--- files/js/main.js
+++ files/js/main.js
@@ -1,4 +1,8 @@
-/*$(".switch").click(function() {
-    $(".sidebar").toggle();
-});*/
 $(".switch").click(function(e){e.preventDefault();$("#change").toggleClass("toggled");});
+$(function() {
+  $("#search-button, #search-icon").click(function(e) {
+    e.preventDefault();
+
+    $("#search-button, #search-icon, #search-form").toggle();
+  });
+});
