76b55eaf2b47930ce095b2f8aa09da21003a41b3
StanMcHow <makhov.stanislav@gmail.com>
2016-04-25 20:39:33 +0300

    Update main.js

--- files/js/main.js
+++ files/js/main.js
@@ -8,3 +8,32 @@ $("#add-medication, .invis").click(function(e) {
     e.preventDefault();
     $("#add-medication, .add-medication").toggle();
   });
+$(".edit").click(function(e) {
+    e.preventDefault();
+    $("#add-medication, .add-medication").toggle();
+  });
+
+
+function admSelectCheck(nameSelect)
+{
+    if(nameSelect){
+        labtypeValue = document.getElementById("labtype").value;
+        if(labtypeValue == nameSelect.value){
+            document.getElementById("labst").style.display = "block";
+            document.getElementById("labsource").style.display = "block";
+            document.getElementById("nonlabs").style.display = "none";
+        }
+        else{
+            document.getElementById("labst").style.display = "none";
+            document.getElementById("labsource").style.display = "none";
+            document.getElementById("nonlabs").style.display = "block";
+        }
+    }
+    else{
+        document.getElementById("labst").style.display = "none";
+        document.getElementById("labsource").style.display = "none";
+        document.getElementById("nonlabs").style.display = "block";
+    }
+}
+
+
