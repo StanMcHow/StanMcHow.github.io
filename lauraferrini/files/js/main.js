89344888920778904062450efb4a02c6ac9dfd29
StanMcHow <makhov.stanislav@gmail.com>
2017-10-30 14:54:13 +0200

    Update main.js

--- lauraferrini/files/js/main.js
+++ lauraferrini/files/js/main.js
@@ -1,8 +1,15 @@
-$(document).ready(function() {  
-    	$('#myCarousel').hammer().on('swipeleft', function(){
-  			$(this).carousel('next'); 
-  		})
-  		$('#myCarousel').hammer().on('swiperight', function(){
-  			$(this).carousel('prev'); 
-  		})
- }); 
+$(".carousel").on("touchstart", function(event){
+        var xClick = event.originalEvent.touches[0].pageX;
+    $(this).one("touchmove", function(event){
+        var xMove = event.originalEvent.touches[0].pageX;
+        if( Math.floor(xClick - xMove) > -5 ){
+            $(this).carousel('next');
+        }
+        else if( Math.floor(xClick - xMove) < 5 ){
+            $(this).carousel('prev');
+        }
+    });
+    $(".carousel").on("touchend", function(){
+            $(this).off("touchmove");
+    });
+});
